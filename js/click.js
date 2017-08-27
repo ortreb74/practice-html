0c2abaa026e6b7a570964cb5b004d0d1b638f52b
pierre donzel <p.donzel-ext@lefebvre-sarrut.eu>
2017-08-27 15:07:01 +0200

    ajout du js le changement de couleur ne marche pas

---
+++ js/click.js
@@ -0,0 +1,8 @@
+function doRectClick(){
+  var myrect = document.getElementById('hexa');
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  myrect.style.fill = 'rgb(' + r + ', ' + g + ' , ' + b + ')';
+  alert("fill");
+}
